cfaf5ec36417ca01af8c008f3ba5c83397016ed5
AdiStas91 <adistas91@yandex.ru>
2020-02-14 21:52:04 +0300

    relise

---
+++ script.js
@@ -0,0 +1,46 @@
+'use strict';
+
+let firstProductPrice = document.querySelector('#first-product__price--input');
+let firstProductWeight = document.querySelector('#first-product__weight--input');
+
+let secondProductPrice = document.querySelector('#second-product__price--input');
+let secondProductWeight = document.querySelector('#second-product__weight--input');
+
+let calculateBtn = document.querySelector('.calculate-btn');
+let resetBtn = document.querySelector('.reset-btn');
+let result = document.querySelector('.result');
+
+const oneKilogram = 1000;
+
+calculateBtn.addEventListener('click', function() {
+	let firstProductPriceValue = firstProductPrice.value;
+	let firstProductWeightValue = firstProductWeight.value;
+	let secondProductPriceValue = secondProductPrice.value;
+	let secondProductWeightValue = secondProductWeight.value;
+
+	let firstProduct = oneKilogram / firstProductWeightValue * firstProductPriceValue;
+	let secondProduct = oneKilogram / secondProductWeightValue * secondProductPriceValue;
+
+	if (firstProduct > secondProduct) {
+		result.textContent = "Цена товара №1 за кг: "
+		+ firstProduct.toFixed(2) + " рублей, а цена товара №2 "
+		+ secondProduct.toFixed(2) + ". Выгоднее купить товар №2";
+	} else if (firstProduct < secondProduct) {
+		result.textContent = "Цена товара №1 за кг: " 
+		+ firstProduct.toFixed(2) + " рублей, а цена товара №2 " 
+		+ secondProduct.toFixed(2) + ". Выгоднее купить товар №1";
+	} else if (firstProductPriceValue === "" || firstProductWeightValue === "" || secondProductPriceValue === "" || secondProductWeightValue) {
+		result.classList.add('alert-text');
+		result.textContent = "Не хватает данных для расчета";
+	} else {
+		result.textContent = "Цена равна, выбирайте который больше нравится";
+	}
+});
+
+resetBtn.addEventListener('click', function() {
+	result.textContent = '';
+	firstProductPrice.value = '';
+	firstProductWeight.value = '';
+	secondProductPrice.value = '';
+	secondProductWeight.value = '';
+});
